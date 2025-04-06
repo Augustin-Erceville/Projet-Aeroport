bf5feb31b2720150432fac539259698bc7d6a18d
Augustin-Erceville <a.erceville@lprs.fr>
2025-04-06 14:15:39 +0200

    V1.6.4.2 • Rédaction du CompagniesRepository.php avec les fonctions Create, Get, GetById, Delete, Update

--- source/repository/CompagniesRepository.php
+++ source/repository/CompagniesRepository.php
@@ -1,1 +1,62 @@
 <?php
+
+class CompagniesRepository {
+     private $pdo;
+
+     public function __construct($pdo) {
+          $this->pdo = $pdo;
+     }
+
+     public function createCompagnie(CompagnieModel $compagnie) {
+          $query = "INSERT INTO compagnies (nom, pays, date_creation) VALUES (:nom, :pays, :date_creation)";
+          $stmt = $this->pdo->prepare($query);
+          $stmt->execute([
+               ':nom' => $compagnie->getNom(),
+               ':pays' => $compagnie->getPays(),
+               ':date_creation' => $compagnie->getDateCreation(),
+          ]);
+          $compagnie->setId($this->pdo->lastInsertId());
+          return $compagnie;
+     }
+
+     public function getCompagnie($nom) {
+          $query = "SELECT * FROM compagnies WHERE nom = :nom";
+          $stmt = $this->pdo->prepare($query);
+          $stmt->execute([':nom' => $nom]);
+          $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+          if ($row) {
+               return new CompagnieModel($row['id'], $row['nom'], $row['pays'], $row['date_creation']);
+          }
+          return null;
+     }
+
+     public function getCompagnieById($id) {
+          $query = "SELECT * FROM compagnies WHERE id = :id";
+          $stmt = $this->pdo->prepare($query);
+          $stmt->execute([':id' => $id]);
+          $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+          if ($row) {
+               return new CompagnieModel($row['id'], $row['nom'], $row['pays'], $row['date_creation']);
+          }
+          return null;
+     }
+
+     public function updateCompagnie(CompagnieModel $compagnie) {
+          $query = "UPDATE compagnies SET nom = :nom, pays = :pays, date_creation = :date_creation WHERE id = :id";
+          $stmt = $this->pdo->prepare($query);
+          $stmt->execute([
+               ':nom' => $compagnie->getNom(),
+               ':pays' => $compagnie->getPays(),
+               ':date_creation' => $compagnie->getDateCreation(),
+               ':id' => $compagnie->getId(),
+          ]);
+     }
+
+     public function deleteCompagnie($id) {
+          $query = "DELETE FROM compagnies WHERE id = :id";
+          $stmt = $this->pdo->prepare($query);
+          $stmt->execute([':id' => $id]);
+     }
+}
