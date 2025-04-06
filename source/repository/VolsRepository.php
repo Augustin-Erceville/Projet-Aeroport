40d1cc1f5d8c3a2875eab1f54dbc1f78c31f0ee7
Augustin-Erceville <a.erceville@lprs.fr>
2025-04-06 14:17:27 +0200

    V1.6.4.3 • Rédaction du VolsRepository.php avec les fonctions Create, Get, GetById, Delete, Update

--- source/repository/VolsRepository.php
+++ source/repository/VolsRepository.php
@@ -1,1 +1,92 @@
 <?php
+
+class VolsRepository {
+     private $pdo;
+
+     public function __construct($pdo) {
+          $this->pdo = $pdo;
+     }
+
+     public function createVol(VolModel $vol) {
+          $query = "INSERT INTO vols (numero_vol, compagnie_id, avion_id, pilote_id, date_depart, date_arrivee, statut) 
+                  VALUES (:numero_vol, :compagnie_id, :avion_id, :pilote_id, :date_depart, :date_arrivee, :statut)";
+          $stmt = $this->pdo->prepare($query);
+          $stmt->execute([
+               ':numero_vol' => $vol->getNumeroVol(),
+               ':compagnie_id' => $vol->getCompagnieId(),
+               ':avion_id' => $vol->getAvionId(),
+               ':pilote_id' => $vol->getPiloteId(),
+               ':date_depart' => $vol->getDateDepart(),
+               ':date_arrivee' => $vol->getDateArrivee(),
+               ':statut' => $vol->getStatut(),
+          ]);
+          $vol->setId($this->pdo->lastInsertId());
+          return $vol;
+     }
+
+     public function getVol($numeroVol) {
+          $query = "SELECT * FROM vols WHERE numero_vol = :numero_vol";
+          $stmt = $this->pdo->prepare($query);
+          $stmt->execute([':numero_vol' => $numeroVol]);
+          $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+          if ($row) {
+               return new VolModel(
+                    $row['id'],
+                    $row['numero_vol'],
+                    $row['compagnie_id'],
+                    $row['avion_id'],
+                    $row['pilote_id'],
+                    $row['date_depart'],
+                    $row['date_arrivee'],
+                    $row['statut']
+               );
+          }
+          return null;
+     }
+
+     public function getVolById($id) {
+          $query = "SELECT * FROM vols WHERE id = :id";
+          $stmt = $this->pdo->prepare($query);
+          $stmt->execute([':id' => $id]);
+          $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+          if ($row) {
+               return new VolModel(
+                    $row['id'],
+                    $row['numero_vol'],
+                    $row['compagnie_id'],
+                    $row['avion_id'],
+                    $row['pilote_id'],
+                    $row['date_depart'],
+                    $row['date_arrivee'],
+                    $row['statut']
+               );
+          }
+          return null;
+     }
+
+     public function updateVol(VolModel $vol) {
+          $query = "UPDATE vols 
+                  SET numero_vol = :numero_vol, compagnie_id = :compagnie_id, avion_id = :avion_id, 
+                      pilote_id = :pilote_id, date_depart = :date_depart, date_arrivee = :date_arrivee, statut = :statut 
+                  WHERE id = :id";
+          $stmt = $this->pdo->prepare($query);
+          $stmt->execute([
+               ':numero_vol' => $vol->getNumeroVol(),
+               ':compagnie_id' => $vol->getCompagnieId(),
+               ':avion_id' => $vol->getAvionId(),
+               ':pilote_id' => $vol->getPiloteId(),
+               ':date_depart' => $vol->getDateDepart(),
+               ':date_arrivee' => $vol->getDateArrivee(),
+               ':statut' => $vol->getStatut(),
+               ':id' => $vol->getId(),
+          ]);
+     }
+
+     public function deleteVol($id) {
+          $query = "DELETE FROM vols WHERE id = :id";
+          $stmt = $this->pdo->prepare($query);
+          $stmt->execute([':id' => $id]);
+     }
+}
